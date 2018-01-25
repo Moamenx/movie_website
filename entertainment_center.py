73b9d2643223a842abf7cf748f3cfda447650bb7
Moamen <moamen_mohammed@hotmail.com>
2018-01-25 15:46:35 +0200

    Styling

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,26 +1,39 @@
 import media
 import learning
 
+# The God Father movie: movie title, movie storyline,
+#  poster image and movie trailer
 the_god_father = media.Movie("The God Father",
-                             "The aging patriarch of an organized crime dynasty transfers control of his clandestine "
+                             "The aging patriarch of an organized crime "
+                             "dynasty transfers control of his clandestine "
                              "empire to his reluctant son.",
                              "thegodfather.jpg",
                              "https://www.youtube.com/watch?v=sY1S34973zA")
-brave_heart = media.Movie("Braveheart"
-                          , "When his secret bride is executed for assaulting an English soldier who tried "
-                            "to rape her, Sir William Wallace begins a revolt against King Edward I of "
-                            "England."
-                          , "braveheart.jpg"
-                          , "https://www.youtube.com/watch?v=1cnoM8EiGGU")
+# Brave Heart movie: movie title, movie storyline,
+#  poster image and movie trailer
+brave_heart = media.Movie("Braveheart",
+                          "When his secret bride is executed for assaulting"
+                          " an English soldier who tried "
+                          "to rape her, Sir William Wallace begins a revolt "
+                          " against King Edward I of England.",
+                          "braveheart.jpg",
+                          "https://www.youtube.com/watch?v=1cnoM8EiGGU")
+# 300 movie: movie title, movie storyline, poster image and movie trailer
 threehundred = media.Movie("300",
-                           "King Leonidas of Sparta and a force of 300 men fight the Persians at Thermopylae in 480 "
-                           "B.C."
-                           , "300.jpg"
-                           , "https://www.youtube.com/watch?v=UrIbxk7idYA")
-messi = media.Movie("Messi"
-                    , "Lionel Messi from early life to international stardom."
-                    , "messi.jpg"
-                    , "https://www.youtube.com/watch?v=wWVvfuZlqSM")
-movies = [the_god_father, messi, threehundred, brave_heart]
+                           "King Leonidas of Sparta and a force of 300 men "
+                           " fight the Persians at Thermopylae in 480 "
+                           "B.C.",
+                           "300.jpg",
+                           "https://www.youtube.com/watch?v=UrIbxk7idYA")
+# Messi movie: movie title, movie storyline, poster image and movie trailer
+messi = media.Movie("Messi",
+                    "Lionel Messi from early life to international stardom.",
+                    "messi.jpg",
+                    "https://www.youtube.com/watch?v=wWVvfuZlqSM")
+movies = [
+    the_god_father,
+    messi,
+    threehundred,
+    brave_heart]
 
 learning.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,10 +1,21 @@
 import webbrowser
-class Movie():
-    def __init__(self, movie_title, story_line,poster_image_url, trailer_youtube_url):
-        self.title = movie_title
-        self.story_line= story_line
-        self.poster_image_url= poster_image_url
-        self.trailer_youtube_url= trailer_youtube_url
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
 
+
+class Movie():
+    """ This is the base attributes of any movie"""
+    def __init__(self,
+                 movie_title,
+                 story_line,
+                 poster_image_url,
+                 trailer_youtube_url):
+        """ The constructor method takes 4 parameters movie title,
+         storyline, poster image url and youtube trailer
+        url """
+        self.title = movie_title
+        self.story_line = story_line
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+
+    def show_trailer(self):
+        """ This method opens the browser's with the movie's trailer"""
+        webbrowser.open(self.trailer_youtube_url)
